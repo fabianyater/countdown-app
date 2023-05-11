48d0b68ac38825daaaabc921718fb0315b068555
Fabian Yate <fabianyate31@gmail.com>
2023-05-11 00:42:21 -0500

    add useEffect :v

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,12 +1,21 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export const Card = () => {
-  const data = JSON.parse(localStorage.getItem("data"));
-  
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    setData(JSON.parse(localStorage.getItem("data")))
+  }, [])
+
   return (
     data &&
     data.map((item, index) => (
-      <div key={index} className="tremor-Card-root relative w-full h-56 text-left ring-1 bg-[#1C1C1C] shadow border-blue-500 ring-gray-200 p-6 rounded-lg flex-1">
+      <div
+        key={index}
+        className="tremor-Card-root relative w-full h-56 text-left ring-1 bg-[#1C1C1C] shadow border-blue-500 ring-gray-200 p-6 rounded-lg flex-1"
+      >
         <div className="flex flex-col items-center justify-center h-full">
           <h2 className="text-2xl font-bold text-center">{item.name}</h2>
           <p>{item.calendar}</p>
