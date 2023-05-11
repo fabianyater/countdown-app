4bb49036ccef1af03cd03e30e467a14d982959f0
Fabian Yate <fabianyater@users.noreply.github.com>
2023-05-11 17:38:42 -0500

    Update ShowTime.jsx

--- app/components/ShowTime.jsx
+++ app/components/ShowTime.jsx
@@ -1,1 +1,8 @@
-
+export const ShowTime = ( {value, type} ) => {
+  return(
+    <div className="text-center">
+      <p className="text-5xl mb-1">{value}</p>
+      <span>{type}</span>
+    </div>
+  )
+}
