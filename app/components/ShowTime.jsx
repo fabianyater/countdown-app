04edc5bb2cd96e3d01f9263e9282992205915e62
Fabian Yate <fabianyater@users.noreply.github.com>
2023-05-11 18:09:44 -0500

    Update ShowTime.jsx

--- app/components/ShowTime.jsx
+++ app/components/ShowTime.jsx
@@ -1,7 +1,10 @@
+import { Monoton } from "next/font/google";
+const mono = Monoton({ subsets: ['latin'], weight: '400' })
+
 export const ShowTime = ( {value, type} ) => {
   return(
     <div className="text-center">
-      <p className="text-5xl mb-1">{value}</p>
+      <p className={`text-5xl mb-1 ${mono.className}`}>{value}</p>
       <span>{type}</span>
     </div>
   )
