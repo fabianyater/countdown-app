48230023f97c0b5b138c96d2168ae201660c05fc
Fabian Yate <fabianyate31@gmail.com>
2023-05-12 00:10:36 -0500

    update input background color

--- app/components/Input.jsx
+++ app/components/Input.jsx
@@ -2,7 +2,7 @@ export  const Input = ({ id, name, label, type, placeholder, value, ...props }) 
   return (
     <fieldset className="mb-6">
       <label htmlFor={id} className="block mb-2 text-xs font-medium text-black dark:text-white">{label}</label>
-      <input type={type} id={id} name={name} placeholder={placeholder} value={value} className="bg-gray-50 border text-gray-950 text-sm rounded-lg block w-full p-2.5 focus:outline-none" required />
+      <input type={type} id={id} name={name} placeholder={placeholder} value={value} className="bg-gray-50 dark:bg-gray-700 text-gray-950 text-sm rounded-lg block w-full p-2.5 focus:outline-none" required />
     </ fieldset>
   )
 }
